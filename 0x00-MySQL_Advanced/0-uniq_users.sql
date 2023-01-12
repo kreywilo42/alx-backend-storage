73d3b96afd8c6c0fe1c75c13ea95242e43de55bc
unknown <kreywilo42@gmail.com>
2023-01-12 16:51:21 +0300

    'mysql_advanced'

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---Creates a table with unique users.
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-	    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	    email VARCHAR(255) NOT NULL UNIQUE,
-	    name VARCHAR(255)
+-- script to create table users if it doesn't exist and add unique constraint to email column
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
--- 0x00-MySQL_Advanced/README.md
+++ 0x00-MySQL_Advanced/README.md
@@ -1,8 +1,1 @@
-# ALX Backend Storage
-
-![Repo size](https://img.shields.io/github/repo-size/Bolaji5821/alx-backend-storage)
-![Pep8 style](https://img.shields.io/badge/PEP8-style%20guide-purple?style=round-square)
-![Latest commit](https://img.shields.io/github/last-commit/Bolaji5821/alx-backend-storage/main?style=round-square)
-
-This repo contains projects for learning backend development data storage concepts.
 0x00. MySQL advanced
